fe8d4ec51e616509c5950e073f5dfe395e48ab5c
masiljangajji <hjk172262@gmail.com>
2023-10-03 20:49:37 +0900

    feat: Unit Class 정의

--- src/main/java/org/nhnacademy/model/type/unitType/Unit.java
+++ src/main/java/org/nhnacademy/model/type/unitType/Unit.java
@@ -1,4 +1,27 @@
 package org.nhnacademy.model.type.unitType;
 
-public interface Unit {
+public class Unit {
+
+    private int damage;
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    private int defense;
+
+    public Unit(int damage, int defense) {
+        this.damage = damage;
+        this.defense = defense;
+    }
+
+
 }
