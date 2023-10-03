2aed56297948b3eb376dfad609e544fbca707e64
masiljangajji <hjk172262@gmail.com>
2023-10-04 00:01:30 +0900

    refactor: Exception 조건 추가 및 logger 자세히

--- src/main/java/org/nhnacademy/service/PlayGame.java
+++ src/main/java/org/nhnacademy/service/PlayGame.java
@@ -8,6 +8,24 @@ import org.slf4j.LoggerFactory;
 
 public class PlayGame {
 
+
+    /**
+     * 종족 선택
+     * <p>
+     * 종족에 따라 무작위로 유닛 생성
+     * <p>
+     * 컴퓨터 종족은 무작위 선택
+     * <p>
+     * 적군과 아군 유닛 표시
+     * <p>
+     * 공격을 실행할 유닛과 받을 유닛 선택
+     * <p>
+     * 컴퓨터는 무작위로 공격을 수행
+     * <p>
+     * 적군의 모든 유닛을 파괴하면 승리
+     */
+
+
     private static final Logger logger = LoggerFactory.getLogger(PlayGame.class);
 
     private static final Scanner sc = new Scanner(System.in);
@@ -19,7 +37,13 @@ public class PlayGame {
         int num = 0;
         try {
             num = sc.nextInt();
-        } catch (InputMismatchException e) {
+
+            if (num < 0 || num > 3) {
+                throw new IllegalArgumentException();
+            }
+
+        } catch (InputMismatchException | IllegalArgumentException e) {
+            logger.warn("{}", e.getMessage());
             logger.warn("{}", Message.INPUT_ERROR);
             logger.info("{}", Message.RETRY_INPUT);
             pickTribe();
