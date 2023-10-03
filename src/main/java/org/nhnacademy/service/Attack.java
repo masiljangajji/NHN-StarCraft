61200ba099cc67791b0df59f055722a4288f55c8
masiljangajji <hjk172262@gmail.com>
2023-10-04 06:19:45 +0900

    feat: 공격기능 추가

--- src/main/java/org/nhnacademy/service/Attack.java
+++ src/main/java/org/nhnacademy/service/Attack.java
@@ -1,9 +1,5 @@
 package org.nhnacademy.service;
 
-import org.nhnacademy.model.type.attackType.FlyableAttack;
-import org.nhnacademy.model.type.attackType.NonFlyableAttack;
-import org.nhnacademy.model.type.unitStatus.Flyable;
-import org.nhnacademy.model.type.unitStatus.NonFlyable;
 import org.nhnacademy.model.unit.Unit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,22 +9,20 @@ public class Attack {
     private static final Logger logger = LoggerFactory.getLogger(Attack.class);
 
 
-    public static void attack(FlyableAttack flyableAttackUnit, Unit defenseUnit) {
-        attackUnit((Unit) flyableAttackUnit, defenseUnit);
+    public static void attack(Unit attackUnit, Unit defenseUnit) {
+
+        if (attackUnit.isAttackFlyable()) {
+            defenseUnit.takeDamage(attackUnit.getDamage());
+        } else {
+
+            if (defenseUnit.isFlyable()) {
+                logger.warn("{}은(는) {}을(를) 공격할 수 없습니다", attackUnit.getClass().getSimpleName(),
+                        defenseUnit.getClass().getSimpleName());
+                throw new IllegalArgumentException();
+            }
+            defenseUnit.takeDamage(attackUnit.getDamage());
+        }
     }
 
-    public static void attack(NonFlyableAttack nonFlyableAttackUnit, NonFlyable nonFlyableUnit) {
-        attackUnit((Unit) nonFlyableAttackUnit, (Unit) nonFlyableUnit);
-    }
-
-    public static void attack(NonFlyableAttack nonFlyableAttackUnit, Flyable flyableUnit) {
-        logger.warn("{}은(는) {}을(를) 공격할 수 없습니다 !", nonFlyableAttackUnit.getClass().getSimpleName(),
-                flyableUnit.getClass().getSimpleName());
-        throw new IllegalArgumentException();
-    }
-
-    public static void attackUnit(Unit attackUnit, Unit defenseUnit) {
-        defenseUnit.takeDamage(attackUnit.getDamage());
-    }
 
 }
