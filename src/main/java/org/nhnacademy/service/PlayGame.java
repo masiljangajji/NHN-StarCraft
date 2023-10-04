423799c9ea1b8c9c41767227704ded9b2a353500
masiljangajji <hjk172262@gmail.com>
2023-10-04 13:15:03 +0900

    refactor: 유닛 선택 및 공격 로직 재정의

--- src/main/java/org/nhnacademy/service/PlayGame.java
+++ src/main/java/org/nhnacademy/service/PlayGame.java
@@ -3,6 +3,7 @@ package org.nhnacademy.service;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 import org.nhnacademy.model.Player;
+import org.nhnacademy.model.unit.Unit;
 import org.nhnacademy.view.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -50,7 +51,6 @@ public class PlayGame {
             pickTribe();
         }
 
-
         return num;
     }
 
@@ -61,12 +61,101 @@ public class PlayGame {
         computer.printUnitList();
     }
 
-    public static void attackEnemy(Player player, Player computer) {
 
-        int playerUnitIndex=selectPlayerUnit(player.getUnitListSize());
-        int computerUnitIndex=selectComputerUnit(computer.getUnitListSize());
+    public static void playerAttack(Player player, Player computer) {
+
+
+        Unit attackUnit = null;
+        Unit defenseUnit = null;
+
+        int playerUnitIndex = 0;
+        int computerUnitIndex = 0;
+
+
+        logger.info(Message.ATTACK_MESSAGE.toString());
+
+        do {
+            playerUnitIndex = selectPlayerUnit(player.getUnitListSize());
+        } while (playerUnitIndex == -1);
+
+
+        do {
+            computerUnitIndex = selectComputerUnit(computer.getUnitListSize());
+        } while (computerUnitIndex == -1);
+
+
+        try {
+
+            attackUnit = player.getUnitByListIndex(playerUnitIndex);
+            defenseUnit = computer.getUnitByListIndex(computerUnitIndex);
+
+            Attack.attack(attackUnit, defenseUnit);
+
+        } catch (IllegalArgumentException e) {
+            logger.warn("{}은(는) {}을(를) 공격할 수 없습니다", attackUnit.getClass().getSimpleName(),
+                    defenseUnit.getClass().getSimpleName());
+            logger.info("{}", Message.RETRY_INPUT);
+            playerAttack(player, computer);
+        }
+
+        if (!defenseUnit.isAlive()) {
+            computer.removeUnitByIndex(computerUnitIndex);
+        }
 
     }
+
+    public static void computerAttack(Player player, Player computer) {
+
+
+        Unit attackUnit = null;
+        Unit defenseUnit = null;
+
+
+        int playerUnitIndex = (int) (Math.random() * player.getUnitListSize() + 1);
+        int computerUnitIndex = (int) (Math.random() * computer.getUnitListSize() + 1);
+
+
+        try {
+
+            attackUnit = computer.getUnitByListIndex(computerUnitIndex);
+            defenseUnit = player.getUnitByListIndex(playerUnitIndex);
+
+            Attack.attack(attackUnit, defenseUnit);
+
+        } catch (IllegalArgumentException e) {
+            playerAttack(player, computer);
+        }
+
+        if (!defenseUnit.isAlive()) {
+            player.removeUnitByIndex(computerUnitIndex);
+        }
+
+    }
+
+
+//
+//        playerUnitIndex = (int) (Math.random() * player.getUnitListSize() + 1);
+//        computerUnitIndex = (int) (Math.random() * computer.getUnitListSize() + 1);
+//
+//        System.out.println(playerUnitIndex + " " + computerUnitIndex);
+//
+//        attackUnit = computer.getUnitByListIndex(computerUnitIndex - 1);
+//        defenseUnit = player.getUnitByListIndex(playerUnitIndex - 1);
+//
+//        try {
+//            player.takeDamageByListIndex(playerUnitIndex - 1, attackUnit.getDamage());
+//            System.out.println("왜 다떄림 대체 왜");
+//        } catch (IllegalArgumentException e) {
+//            attack(player, computer, userAttack);
+//        }
+//
+//        logger.info("{}이(가) {}을(를) 공격\n남은 체력 {}", attackUnit.getClass().getSimpleName(),
+//                defenseUnit.getClass().getSimpleName(), defenseUnit.getDefense());
+//
+//        if (!defenseUnit.isAlive()) {
+//            player.removeUnitByIndex(playerUnitIndex - 1);
+//        }
+
 
     public static int selectPlayerUnit(int playerUnitMaxIndex) {
         int playerUnitIndex = 0;
@@ -80,14 +169,15 @@ public class PlayGame {
         } catch (IllegalArgumentException e) {
             logger.warn("{}", Message.INPUT_RANGE_ERROR);
             logger.info("{}", Message.RETRY_INPUT);
-            selectPlayerUnit(playerUnitMaxIndex);
+            sc.nextLine();
+            return -1;
         } catch (InputMismatchException e) {
             logger.warn("{}", Message.ONLY_NUMBER);
             logger.info("{}", Message.RETRY_INPUT);
             sc.nextLine();
-            selectPlayerUnit(playerUnitMaxIndex);
+            return -1;
         }
-        return playerUnitIndex;
+        return playerUnitIndex - 1;
     }
 
     public static int selectComputerUnit(int computerUnitMaxIndex) {
@@ -102,14 +192,15 @@ public class PlayGame {
         } catch (IllegalArgumentException e) {
             logger.warn("{}", Message.INPUT_RANGE_ERROR);
             logger.info("{}", Message.RETRY_INPUT);
-            selectComputerUnit(computerUnitMaxIndex);
+            sc.nextLine();
+            return -1;
         } catch (InputMismatchException e) {
             logger.warn("{}", Message.ONLY_NUMBER);
             logger.info("{}", Message.RETRY_INPUT);
             sc.nextLine();
-            selectComputerUnit(computerUnitMaxIndex);
+            return -1;
         }
-        return computerUnitIndex;
+        return computerUnitIndex - 1;
     }
 
 }
