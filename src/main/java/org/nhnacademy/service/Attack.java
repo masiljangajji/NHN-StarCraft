cc0c6b6c6ad45c70adce4c71315261e7cc582ddf
masiljangajji <hjk172262@gmail.com>
2023-10-04 13:12:51 +0900

    refactor: attack 메서드 재정의

--- src/main/java/org/nhnacademy/service/Attack.java
+++ src/main/java/org/nhnacademy/service/Attack.java
@@ -1,23 +1,18 @@
 package org.nhnacademy.service;
 
+import org.nhnacademy.model.type.attackType.FlyableAttack;
+import org.nhnacademy.model.type.unitStatus.Flyable;
 import org.nhnacademy.model.unit.Unit;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Attack {
-
-    private static final Logger logger = LoggerFactory.getLogger(Attack.class);
 
 
     public static void attack(Unit attackUnit, Unit defenseUnit) {
 
-        if (attackUnit.isAttackFlyable()) {
+        if (attackUnit instanceof FlyableAttack) {
             defenseUnit.takeDamage(attackUnit.getDamage());
         } else {
-
-            if (defenseUnit.isFlyable()) {
-                logger.warn("{}은(는) {}을(를) 공격할 수 없습니다", attackUnit.getClass().getSimpleName(),
-                        defenseUnit.getClass().getSimpleName());
+            if (defenseUnit instanceof Flyable) {
                 throw new IllegalArgumentException();
             }
             defenseUnit.takeDamage(attackUnit.getDamage());
@@ -26,3 +21,6 @@ public class Attack {
 
 
 }
+
+
+
