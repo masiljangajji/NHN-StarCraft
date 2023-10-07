a7a1e4161135e6b0bbdd8c95df045394b9d5586e
masiljangajji <hjk172262@gmail.com>
2023-10-07 21:25:41 +0900

    refactor: enum message 변경

--- src/main/java/org/nhnacademy/view/Message.java
+++ src/main/java/org/nhnacademy/view/Message.java
@@ -3,7 +3,7 @@ package org.nhnacademy.view;
 public enum Message {
 
 
-    PICK_TRIBE("종족을 선택해 주세요\n1 : Terran\n2 : Zerg\n3 : Protos"),
+    PICK_TRIBE("종족을 선택해 주세요\n1 : Protos\n2 : Terran\n3 : Zerg"),
 
     INPUT_RANGE_ERROR("입력 범위를 벗어났습니다!"),
 
@@ -13,18 +13,18 @@ public enum Message {
     ONLY_NUMBER("입력은 숫자만 가능합니다 !"),
 
     PLAYER_UNIT("Player 유닛"),
-    COMPUTER_UNIT("Computer 유닛"),
+    OPPONENT_UNIT("Opponent 유닛"),
 
     ATTACK_MESSAGE("공격을 수행할 아군 유닛과 공격할 적군 유닛을 선택하세요"),
 
     SELECT_PLAYER_UNIT("공격할 아군 유닛을 선택해 주세요"),
     SELECT_ENEMY_UNIT("공격할 적군 유닛을 선택해 주세요"),
     PLAYER_LOSE("Player의 모든 Unit이 파괴 됐습니다!\nPlayer 패배"),
-    COMPUTER_LOSE("Computer의 모든 Unit이 파괴 됐습니다\nComputer 패배"),
+    COMPUTER_LOSE("Opponent의 모든 Unit이 파괴 됐습니다\nOpponent 패배"),
     END_GAME("게임을 종료합니다."),
 
-    PLAYER_DESICION_LOSE("플레이어가 공격 할 수 있는 유닛이 없습니다.\nPlayer 패배"),
-    COMPUTER_DESICION_LOSE("컴퓨터가 공격 할 수 있는 유닛이 없습니다\nComputer 패배");
+    PLAYER_DESICION_LOSE("Player는 공중 공격 유닛이 없습니다.\nPlayer 패배"),
+    OPPONENT_DESICION_LOSE("Opponent는 공중 공격 유닛이 없습니다\nOpponent 패배");
 
     private final String message;
 
