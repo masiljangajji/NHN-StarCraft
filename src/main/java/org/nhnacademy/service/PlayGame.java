6f3efba262e5bd16b63e3bfe3b016af05148b805
masiljangajji <hjk172262@gmail.com>
2023-10-08 03:46:05 +0900

    refactor: selectUnit 공격유닛 , 방어유닛 체크로직 추가

--- src/main/java/org/nhnacademy/service/PlayGame.java
+++ src/main/java/org/nhnacademy/service/PlayGame.java
@@ -4,6 +4,7 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 import org.nhnacademy.model.Player;
 import org.nhnacademy.model.unit.Unit;
+import org.nhnacademy.view.ErrorMessage;
 import org.nhnacademy.view.Message;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,7 +20,7 @@ public class PlayGame {
 
 
     public static int pickTribe() {
-        logger.info("{}", Message.PICK_TRIBE);
+        logger.info(Message.PICK_TRIBE.toString());
 
         int num = 0;
         try {
@@ -30,12 +31,12 @@ public class PlayGame {
             }
 
         } catch (IllegalArgumentException e) {
-            logger.warn("{}", Message.INPUT_RANGE_ERROR);
-            logger.info("{}", Message.RETRY_INPUT);
+            logger.warn(ErrorMessage.INPUT_RANGE.toString());
+            logger.info(Message.RETRY_INPUT.toString());
             return -1;
         } catch (InputMismatchException e) {
-            logger.warn("{}", Message.ONLY_NUMBER);
-            logger.info("{}", Message.RETRY_INPUT);
+            logger.warn(ErrorMessage.INPUT_NUMBER.toString());
+            logger.info(Message.RETRY_INPUT.toString());
             sc.nextLine();
             return -1;
         }
@@ -44,9 +45,9 @@ public class PlayGame {
     }
 
     public static void printUnitList(Player player, Player opponent) {
-        logger.info("\n{}", Message.PLAYER_UNIT);
+        logger.info(Message.PLAYER_UNIT.toString());
         player.printUnitList();
-        logger.info("{}", Message.OPPONENT_UNIT);
+        logger.info(Message.OPPONENT_UNIT.toString());
         opponent.printUnitList();
     }
 
@@ -62,15 +63,15 @@ public class PlayGame {
 
         if (check) {
 
-            logger.info(Message.ATTACK_MESSAGE.toString());
+            logger.info(Message.PICK_ATTACK_AND_DEFENSE_UNIT.toString());
 
             do {
-                attackPlayerUnitIndex = selectUnit(attackPlayer.getUnitListSize());
+                attackPlayerUnitIndex = selectUnit(attackPlayer.getUnitListSize(), true);
             } while (attackPlayerUnitIndex == -1);
 
 
             do {
-                defensePlayerUnitIndex = selectUnit(defensePlayer.getUnitListSize());
+                defensePlayerUnitIndex = selectUnit(defensePlayer.getUnitListSize(), false);
             } while (defensePlayerUnitIndex == -1);
 
             attackUnit = attackPlayer.getUnitByListIndex(attackPlayerUnitIndex);
@@ -79,11 +80,12 @@ public class PlayGame {
             try {
                 attackUnit.attack(defenseUnit);
             } catch (IllegalArgumentException e) {
-                logger.warn("{}은(는) {}을(를) 공격할 수 없습니다", attackUnit.getClass().getSimpleName(),
-                        defenseUnit.getClass().getSimpleName());
-                logger.info("{}", Message.RETRY_INPUT);
+                logger.warn("{}은(는) {}을(를) 공격할 수 없습니다", attackUnit.getUnitName(),
+                        defenseUnit.getUnitName());
+                logger.info(Message.RETRY_INPUT.toString());
                 return false;
             }
+
         } else {
 
             attackPlayerUnitIndex = (int) (Math.random() * attackPlayer.getUnitListSize());
@@ -98,11 +100,10 @@ public class PlayGame {
                 return false;
             }
 
-
         }
 
-        logger.info("\n{}이(가)  {}을(를) 공격했습니다   남은 방어력{}", attackUnit.getClass().getSimpleName(),
-                defenseUnit.getClass().getSimpleName(), defenseUnit.getDefense());
+        logger.info("\n{}이(가)  {}을(를) 공격했습니다   남은 방어력{}", attackUnit.getUnitName(),
+                defenseUnit.getUnitName(), defenseUnit.getDefense());
 
         if (!defenseUnit.isAlive()) {
             defensePlayer.removeUnitByIndex(defensePlayerUnitIndex);
@@ -112,26 +113,32 @@ public class PlayGame {
     }
 
 
-    public static int selectUnit(int unitMaxIndex) {
+    public static int selectUnit(int unitMaxIndex, boolean pickMyUnit) {
         int playerUnitIndex = 0;
 
-        logger.info("{}", Message.SELECT_PLAYER_UNIT);
+        if (pickMyUnit) {
+            logger.info(Message.SELECT_PLAYER_UNIT.toString());
+        } else {
+            logger.info(Message.SELECT_ENEMY_UNIT.toString());
+        }
         try {
             playerUnitIndex = sc.nextInt();
             if (playerUnitIndex < 1 || playerUnitIndex > unitMaxIndex) {
                 throw new IllegalArgumentException();
             }
         } catch (IllegalArgumentException e) {
-            logger.warn("{}", Message.INPUT_RANGE_ERROR);
-            logger.info("{}", Message.RETRY_INPUT);
+            logger.warn(ErrorMessage.INPUT_RANGE.toString());
+            logger.info(Message.RETRY_INPUT.toString());
             sc.nextLine();
             return -1;
         } catch (InputMismatchException e) {
-            logger.warn("{}", Message.ONLY_NUMBER);
-            logger.info("{}", Message.RETRY_INPUT);
+            logger.warn(ErrorMessage.INPUT_NUMBER.toString());
+            logger.info(Message.RETRY_INPUT.toString());
             sc.nextLine();
             return -1;
         }
+
+
         return playerUnitIndex - 1;
     }
 
@@ -145,9 +152,9 @@ public class PlayGame {
         if (!isPlayerFlyableAttack && isOpponentFlyableUnit) { // player 공중공격 못하는데 , opponent는 공중유닛 있어
 
             if (check) {
-                logger.info("{}", Message.PLAYER_DESICION_LOSE);
+                logger.info(Message.PLAYER_DESICION_LOSE.toString());
             } else {
-                logger.info("{}", Message.OPPONENT_DESICION_LOSE);
+                logger.info(Message.OPPONENT_DESICION_LOSE.toString());
             }
 
             return true;
